07ab43d9613efde6c8ca9b5892975ad7966200f1
tnauss <nausst@gmail.com>
2015-02-18 14:29:51 +0100

    Minor: Rename package

---
+++ R/satellite-package.R
@@ -0,0 +1,16 @@
+#' Smorgasboard for remote sensing functions
+#'
+#' The package provides a variety of functions which are usefull for 
+#' handling, manipulating and visualizing remote sensing data.
+#'
+#' @name satellite-package
+#' @aliases satellite
+#' @docType package
+#' @title Smorgasboard for remote sensing functions
+#' @author Hanna Meyer, Thomas Nauss\cr
+#' \cr
+#' \emph{Maintainer:} Environmental Informatics \email{admin@@environmentalinformatics-marburg.de}
+#'
+#' @keywords package
+#'
+NULL
