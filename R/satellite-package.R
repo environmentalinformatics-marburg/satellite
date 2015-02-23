904b151e76069edb3924ef12c4b12b1ffd2de680
tnauss <nausst@gmail.com>
2015-02-23 13:52:22 +0100

    Fix dependencies to make function satellite() work

--- R/satellite-package.R
+++ R/satellite-package.R
@@ -4,14 +4,17 @@
 #' handling, manipulating and visualizing remote sensing data.
 #'
 #' @name satellite-package
-#' @aliases satellite
+#' @aliases satellitepackage
 #' @docType package
 #' @title Smorgasboard for remote sensing functions
 #' @author Thomas Nauss, Hanna Meyer\cr
 #' \cr
 #' \emph{Maintainer:} Environmental Informatics \email{admin@@environmentalinformatics-marburg.de}
 #'
+#' @import raster
+#' 
 #' @references Some functions are taken and/or addopted from Sarah C. Goslee (2011). Analyzing Remote Sensing Data in R: The landsat Package. Journal of Statistical Software,43(4), 1-25. URL \url{http://www.jstatsoft.org/v43/i04/}.
+#' 
 #' @keywords package
 #'
 NULL
--- R/satellite.R
+++ R/satellite.R
@@ -1,12 +1,53 @@
-
-
 if ( !isGeneric("satellite") ) {
   setGeneric("satellite", function(filepath, ...)
     standardGeneric("satellite"))
 }
 
+#' Create a Satellite object
+#'
+#' @description
+#' Method to create a Satellite Object
+#' 
+#' @return Satellite object
+#' 
+#' @export satellite
+#' 
+#' @details Computation of ESun is taken from Updike and Comp (2011). Sun-earth
+#' distance is computed using \code{\link{earthSun}}.
+#' 
+#' @references The formulas are taken from the following sources:
+#' 
+#' Spencer: Spencer JW (1971) Fourier series representation of the position of 
+#' the sun. Search 2/5. Taken from 
+#' \url{http://www.mail-archive.com/sundialuni-koeln.de/msg01050.html}. See
+#' also: Bird R, Riordan C (1984) Simple solar spectral model for direct and 
+#' diffuse irradiance on horizontal and tilted planes at the Earth's surface for
+#' cloudless atmospheres. \url{http://www.nrel.gov/docs/legosti/old/2436.pdf}.
+#' 
+#' Mather:  Paul M. Mather (2005) Computer Processing of Remotely-Sensed Images:
+#' An Introduction. Wiley, ISBN: 978-0-470-02101-9, 
+#' \url{http://eu.wiley.com/WileyCDA/WileyTitle/productCd-0470021012.html}.
+#' 
+#' ESA: ESA Earth Observation Quality Control: Landsat frequently asked questions. 
+#' 
+#' @seealso \code{\link{earthSun}} for calculating the sun-earth distance based
+#' on the day of the year and \code{\link{eSun}} for wrapping this function and
+#' alternative derivation of ESun.
+#' 
+#' @name satellite
+#' 
+#' @examples
+#' satellite(date = "2015-01-01", formula = "Spencer")
+#' 
+NULL
+
+
+# Function using filepath ------------------------------------------------------
+#' @param filepath full path and name of the satellite data file
+#'
+#' @rdname satellite
 setMethod("satellite", 
           signature(filepath = "character"), 
           function(filepath){
-            new("SatelliteFilepath", filepath = filepath)
+            return(new("Satellite", filepath = filepath))
           })
