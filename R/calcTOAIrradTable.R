7e609c39c6626a51e538c47cdb5ce509d3c3d855
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-07-02 20:37:39 +0200

    Revised version.

--- R/calcTOAIrradTable.R
+++ R/calcTOAIrradTable.R
@@ -7,13 +7,14 @@ if ( !isGeneric("calcTOAIrradTable") ) {
 #' @description
 #' Get mean extraterrestrial solar irradiance (ESun) using published values.
 #' 
-#' @param x An object of type Satellite or sensor id ("LC4/5/7") as character
-#' @param normalize Normalize ESun to mean earth sun distance, TRUE or FALSE
+#' @param x A Satellite object or sensor id ("LC4/5/7") as character.
+#' @param normalize Logical; if \code{TRUE}, ESun is normalized to mean 
+#' earth-sun distance. 
 #' @param esd Earth-sun distance (AU, can be estimated using 
 #' \code{\link{calcEarthSunDist}}). If x is a Satellite object and esd is not 
 #' supplied and necessary for normalization, it is tried to take it from the 
 #' metadata, otherwise it is estimated by the day of the year using 
-#' \code{\link{calcEartSunDist}}.
+#' \code{\link{calcEarthSunDist}}.
 #'
 #' @export calcTOAIrradTable
 #' 
@@ -21,29 +22,29 @@ if ( !isGeneric("calcTOAIrradTable") ) {
 #' 
 #' @details Currently implemented sensors are Landsat 4, 5 and 7.
 #' 
-#' If results should not be normalized to a mean earth sun distance, the 
-#' actual earth sun distance is approximated by the day of the year using
-#' \code{\link{calcEartSunDist}}.
+#' If results should not be normalized to a mean earth-sun distance, the 
+#' actual earth-sun distance is approximated by the day of the year using
+#' \code{\link{calcEarthSunDist}}.
 #' 
 #' @references  Tabulated values of the solar irradiance for Landsat 4 and 5 are 
 #' taken from Chander G, Markham B (2003) Revised Landsat-5 TM radiometric 
-#' calibration procedures and postcalibration dynamic ranges.  IEEE Transaction 
-#' on Geoscience and Remote Sensing 41/11, doi:10.1109/LGRS.2007.898285, URL
-#' \url{http://landsathandbook.gsfc.nasa.gov/pdfs/L5TMLUTIEEE2003.pdf}.
+#' calibration procedures and postcalibration dynamic ranges.  IEEE Transactions 
+#' on Geoscience and Remote Sensing 41/11, doi:10.1109/LGRS.2007.898285, online 
+#' available at \url{http://landsathandbook.gsfc.nasa.gov/pdfs/L5TMLUTIEEE2003.pdf}.
 #' 
 #' Tabulated values of the solar irradiance for Landsat 7 are taken from
 #' \href{http://landsathandbook.gsfc.nasa.gov/pdfs/Landsat7_Handbook.pdf}{NASA's
-#' Landsat7 handbook, tab 11.3 (Thuillier spectrum)}
+#' Landsat7 handbook, tab 11.3 (Thuillier spectrum)}. 
 #' 
 #' @seealso \code{\link{calcTOAIrradRadRef}} for the computation of the solar 
 #' irradiance based on maximum radiation and reflection values of the dataset or
 #' \code{\link{calcTOAIrradModel}} for the computation of the solar irradiance 
-#' based on look-up tables for the sensor's relative spectral resonse and solar 
+#' based on look-up tables for the sensor's relative spectral response and solar 
 #' irradiation spectral data.
 #' 
 #' See \code{\link{calcEarthSunDist}} for calculating the earth-sun
 #' distance based on the day of the year which is called by this function if
-#' ESun should be corrected for actual earth sun distance.
+#' ESun should be corrected for actual earth-sun distance.
 #' 
 #' @examples
 #' path <- system.file("extdata", package = "satellite")
@@ -51,8 +52,8 @@ if ( !isGeneric("calcTOAIrradTable") ) {
 #' sat <- satellite(files)
 #' calcTOAIrradTable(sat)
 #'  
-#' calcTOAIrradTable(sensor = "LC7", normalize = FALSE, 
-#' calcEartSunDist("2015-01-01"))
+#' calcTOAIrradTable(x = "LC7", normalize = FALSE, 
+#'                   calcEarthSunDist("2015-01-01"))
 #' 
 NULL
 
@@ -66,9 +67,9 @@ setMethod("calcTOAIrradTable",
           signature(x = "Satellite"), 
           function(x, normalize = TRUE, esd){
             if(normalize == FALSE & missing(esd)){
-              esd = getSatESD(x)
+              esd <- getSatESD(x)
               if(is.na(esd)){
-                esd = calcEartSunDist(date)
+                esd <- calcEarthSunDist(date)
               } 
             }
             if(normalize == TRUE){
@@ -79,7 +80,7 @@ setMethod("calcTOAIrradTable",
                                         normalize  = normalize, 
                                         esd = esd)
             }
-            bcde = names(esun)
+            bcde <- names(esun)
             x <- addSatMetaParam(x, 
                                  meta_param = data.frame(
                                    BCDE = bcde,
