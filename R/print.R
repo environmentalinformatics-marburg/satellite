1dc71c8bf3bca9badf83015d4d92698051a1f36a
Hanna <hanna.meyer1@gmail.com>
2015-07-10 12:07:08 +0200

    information on reference system included in print function

--- R/print.R
+++ R/print.R
@@ -1,6 +1,14 @@
 setMethod ('print' , signature(x= "Satellite"), 
            function(x)
            {
-             show(x)  
+             cat(paste("Summary of the Satellite Object\n\n", sep = ""))
+             print(getSatMeta(x)[1:10]) 
+             cat(paste("\n Layers are projected in:\n",
+                       proj4string(getSatDataLayers(sat)[[1]]), 
+                       sep = ""))
+             if (length(unique(lapply(getSatDataLayers(sat),
+                                      proj4string))[[1]])>1){
+               print("Warning: Layers are not in same projection")
+             }
            }
 )
--- R/show.R
+++ R/show.R
@@ -1,7 +1,6 @@
 setMethod("show", signature(object = "Satellite"), 
           function(object)
           {
-            cat(paste("Summary of the Satellite Object\n", sep = ""))
-            print(getSatMeta(object)[1:10])
+           print(object)
           }
 )
