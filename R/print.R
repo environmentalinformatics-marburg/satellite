5e652fa54fad06955eb09dea48bb890c4cf437ce
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-07-14 11:48:45 +0200

    Standardized input parameter.

--- R/print.R
+++ R/print.R
@@ -1,15 +1,15 @@
-setMethod ('print' , signature(x= "Satellite"), 
+setMethod ('print' , signature(x = "Satellite"), 
            function(x)
            {
              cat(paste("Summary of the Satellite Object\n\n", sep = ""))
              print(getSatMeta(x)[1:10]) 
              cat(paste("\n Layers are projected in:\n",
-                       getSatProjection(sat, getSatBCDE(sat, 1)), 
+                       getSatProjection(x, getSatBCDE(x, 1)), 
                        sep = ""))
              if (length(unique(as.character(lapply(
-               seq(countSatDataLayers(sat)), 
-               function(i) getSatProjection(sat, 
-                                            getSatBCDE(sat, i)))))) > 1) {
+               seq(countSatDataLayers(x)), 
+               function(i) getSatProjection(x, 
+                                            getSatBCDE(x, i)))))) > 1) {
                cat("\n\nWarning: Not all layers have same projection")
              }
            }
