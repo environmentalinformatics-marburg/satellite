dc1465438e82efa18761f99c2657e280681a9f2e
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-03-17 09:55:37 +0100

    Introduced calculation of the incidence angle based on radians only; implemented 'POSIX*', 'character' and 'Raster' methods; the output is now returned either in radians (default) or in degrees.

--- R/declination.R
+++ R/declination.R
@@ -6,14 +6,17 @@
 #' 
 #' @param n Day of the year (DOY) as \code{integer} or \code{Date} from which to
 #' derive DOY.
-#' @param formula Formula to be applied, specified through the name of the 
-#' author (see References). Currently, the approximation equation of Cooper 
-#' (1969) and the more accurate estimation of Spencer (1971) are implemented.
+#' @param formula \code{character}, formula from which to calculate the angular 
+#' declination of the sun, specified through the name of the author (see 
+#' References). Currently, the approximation equation from Cooper (1969) and the 
+#' more accurate version from Spencer (1971) are available.
+#' @param unit \code{character}, determines whether to return the declination 
+#' angle in radians (default) or degrees.
 #' 
 #' @return 
 #' A \code{numeric} declination angle.
 #' 
-#' @details 
+#' @note  
 #' "The declination of the sun is the angle between the equator and a line drawn 
 #' from the centre of the Earth to the centre of the sun." (directly taken from 
 #' \url{http://www.pveducation.org/pvcdrom/properties-of-sunlight/declination-angle})
@@ -32,18 +35,21 @@
 #'   \item Duffie JA, Beckman WA (2013) Solar Engineering of Thermal Processes. 
 #'   Wiley: Hoboken, New Jersey, ISBN: 978-0-470-87366-3,
 #'   \url{http://eu.wiley.com/WileyCDA/WileyTitle/productCd-0470873663.html} and 
-#'   \item ITACA (2016) Part 3: Calculating Solar Angles, 
+#'   \item ITACA (2016) Part 3: Calculating Solar Angles. Available online at  
 #'   \url{http://www.itacanet.org/the-sun-as-a-source-of-energy/part-3-calculating-solar-angles/}
 #' }
 #' for further information.
 #' 
 #' @examples 
-#' declination(Sys.Date())
-#' declination(Sys.Date(), formula = "Spencer")
+#' ## calculate current declination angles
+#' sapply(c("Cooper", "Spencer"), function(frml) {
+#'   declination(Sys.Date(), formula = frml)
+#' })
 #' 
 #' @export declination
 #' @name declination
-declination <- function(n, formula = c("Cooper", "Spencer")) {
+declination <- function(n, formula = c("Cooper", "Spencer"), 
+                        unit = c("radians", "degrees")) {
   
   ## if 'Date' input, retrieve day of the year
   if (class(n) == "Date")
@@ -51,17 +57,23 @@ declination <- function(n, formula = c("Cooper", "Spencer")) {
   
   ## formula by Cooper 1969  
   if (formula[1] == "Cooper") {
-    23.45 * sin(2 * pi * (284 + n) / 365)
+    dcl <- 23.45 * sin(2 * pi * (284 + n) / 365)
     
   ## formula by Spencer 1971  
   } else if (formula[1] == "Spencer") {
     B <- (n - 1) * 2 * pi / 365
-    (180 / pi) * (0.006918 - 0.399912 * cos(B) + 0.070257 * sin(B) - 
-                    0.006758 * cos(2 * B) + 0.000907 * sin(2 * B) - 
-                    0.002697 * cos(3 * B) + 0.00148 * sin(3 * B))
+    dcl <- (180 / pi) * (0.006918 - 0.399912 * cos(B) + 0.070257 * sin(B) - 
+                           0.006758 * cos(2 * B) + 0.000907 * sin(2 * B) - 
+                           0.002697 * cos(3 * B) + 0.00148 * sin(3 * B))
     
   ## stop if no valid formula is specified  
   } else {
     stop("Please specify a valid 'formula' argument (see ?declination).\n")
   }
+  
+  ## convert to degrees (optional) and return
+  if (unit[1] == "radians")
+    return(dcl * pi / 180)
+  else
+    return(dcl)
 }
--- R/solarIncidenceAngle.R
+++ R/solarIncidenceAngle.R
@@ -1,37 +1,45 @@
-#' Compute solar incidence and hour angle
+if ( !isGeneric("solarIncidenceAngle") ) {
+  setGeneric("solarIncidenceAngle", function(x, ...)
+    standardGeneric("solarIncidenceAngle"))
+}
+#' Compute solar incidence angle
 #'
 #' @description
 #' Compute the solar incidence angle over non-planar surfaces following the
-#' equation from Duffie and Beckman (2013) and the solar hour angle defined as 
-#' the time difference from noon times 15 degrees per hour.
+#' equation from Duffie and Beckman (2013).
 #'
-#' @param x A date-time object of class \code{POSIX*} or, alternatively, a
-#' \code{character} object that can be coerced by \code{\link{strptime}}.
+#' @param x A date-time object of class \code{POSIX*}, or a \code{character} 
+#' object that can be coerced by \code{\link{strptime}}, or a \code{Raster*} 
+#' object holding such information.
 #' @param dem \code{RasterLayer}, digital elevation model in EPSG:4326
-#' \url{http://spatialreference.org/ref/epsg/wgs-84/}.
-#' @param formula Formula to deploy for the calculation of the angular
+#' (see \url{http://spatialreference.org/ref/epsg/wgs-84/}).
+#' @param formula \code{character}, formula from which to calculate the angular
 #' declination of the sun, see \code{\link[satellite]{declination}}.
-#' @param neighbors \code{integer}, number of focal cells to use for the
-#' computation of surface slope and aspect, see \code{\link{terrain}}.
-#' @param unit \code{character}, determines whether the solar incidence angle is 
-#' returned in radians (default) or degrees.
-#' @param ... If \code{x} is a \code{character} object, further arguments passed
-#' on to \code{\link{strptime}}.
+#' @param neighbors \code{integer}, number of focal cells from which to 
+#' calculate surface slope and aspect, see \code{\link{terrain}}.
+#' @param unit \code{character}, determines whether to return the solar 
+#' incidence angle in radians (default) or degrees.
+#' @param ... If \code{x} is a \code{character} object, formatting arguments 
+#' passed on to \code{\link{strptime}}, or if \code{x} is a \code{Raster*} 
+#' object, formatting arguments related with the specified \code{origin}.
+#' @param origin A \code{POSIX*} or \code{character} object that defines the 
+#' origin of \code{x}, see \code{\link{solarHourAngle}}.
 #'
 #' @return
-#' \itemize{
-#' \item \emph{solarIncidenceAngle}: A \code{RasterLayer} with pixel-wise solar 
-#' incidence angles.
-#' \item \emph{hourAngle}: A \code{numeric} solar hour angle.
-#' }
+#' A \code{Raster*} object with pixel-wise solar incidence angles.
 #'
+#' @note 
+#' An angle of more than 90 degrees, or likewise \code{pi/2}, indicates that the 
+#' sun is behind the surface.
+#' 
 #' @references
 #' Duffie JA, Beckman WA (2013) Solar Engineering of Thermal Processes. Wiley:
 #' Hoboken, New Jersey, ISBN: 978-0-470-87366-3,
 #' \url{http://eu.wiley.com/WileyCDA/WileyTitle/productCd-0470873663.html}.
 #'
 #' @seealso
-#' \code{\link[satellite]{declination}}.
+#' \code{\link[satellite]{declination}}, \code{\link{solarHourAngle}}, 
+#' \code{\link{terrain}}.
 #'
 #' @examples 
 #' ## import sample data
@@ -42,21 +50,20 @@
 #' solarIncidenceAngle(Sys.time(), dem, formula = "Spencer", 
 #'                     unit = "degrees")
 #' 
-#' ## current solar hour angle
-#' hourAngle(Sys.time())
-#' 
 #' @export solarIncidenceAngle
 #' @name solarIncidenceAngle
-solarIncidenceAngle <- function(x, dem, formula = c("Cooper", "Spencer"),
-                                neighbors = 8L, unit = c("radians", "degrees"), 
-                                ...) {
+NULL
 
-  ## convert 'x' to 'POSIXt' object (optional)
-  if (!any(class(x) == "POSIXt"))
-    x <- strptime(x, ...)
+### 'POSIXt' method ----- 
+#' @aliases solarIncidenceAngle,POSIXt-method
+#' @rdname solarIncidenceAngle
+setMethod("solarIncidenceAngle", 
+          signature(x = "POSIXt"), 
+          function(x, dem, formula = c("Cooper", "Spencer"),
+                   neighbors = 8L, unit = c("radians", "degrees")) {
 
   ## compute angular declination of the sun
-  delta <- declination(as.Date(x), formula = formula[1])
+  delta <- declination(as.Date(x), formula = formula[1], unit = "radians")
 
   ## pixel-wise latitude
   yrs <- raster::res(dem)[2]
@@ -68,13 +75,15 @@ solarIncidenceAngle <- function(x, dem, formula = c("Cooper", "Spencer"),
   phi <- raster::raster(mat, template = dem)
 
   ## compute surface slope and aspect
-  slp <- raster::terrain(dem, opt = "slope", neighbors = neighbors)
+  slp <- raster::terrain(dem, opt = "slope", neighbors = neighbors, 
+                         unit = "radians")
 
-  asp <- raster::terrain(dem, opt = "aspect", neighbors = neighbors)
+  asp <- raster::terrain(dem, opt = "aspect", neighbors = neighbors, 
+                         unit = "radians")
   asp <- raster::calc(asp, fun = function(x) x - pi)
 
-  ## compute hour angle
-  omega <- hourAngle(x)
+  ## compute solar hour angle
+  omega <- solarHourAngle(x, unit = "radians")
 
   ## compute solar incidence angle
   theta <- acos(sin(delta) * sin(phi) * cos(slp) - 
@@ -88,25 +97,72 @@ solarIncidenceAngle <- function(x, dem, formula = c("Cooper", "Spencer"),
     return(theta * 180 / pi)
   else
     return(theta)
-}
+})
 
-
-### compute hour angle ---------------------------------------------------------
+### 'character' method ----- 
+#' @aliases solarIncidenceAngle,character-method
 #' @rdname solarIncidenceAngle
-#' @export hourAngle
-hourAngle <- function(x, ...) {
+setMethod("solarIncidenceAngle", 
+          signature(x = "character"), 
+          function(x, dem, formula = c("Cooper", "Spencer"),
+                   neighbors = 8L, unit = c("radians", "degrees"), 
+                   ...) {
+            
+  ## convert to 'POSIXt' object
+  x <- strptime(x, ...)
+  
+  ## calculate solar incidence angle
+  solarIncidenceAngle(x, dem, formula[1], neighbors, unit[1])
+})
+          
+### 'Raster' method -----
+#' @aliases solarIncidenceAngle,Raster-method
+#' @rdname solarIncidenceAngle
+setMethod("solarIncidenceAngle", 
+          signature(x = "Raster"), 
+          function(x, origin = strptime("1993-01-01", "%Y-%m-%d"), dem, 
+                   formula = c("Cooper", "Spencer"), neighbors = 8L, 
+                   unit = c("radians", "degrees"), ...) {
 
-  ## convert 'x' to 'POSIXt' object (optional)
-  if (!any(class(x) == "POSIXt"))
-    x <- strptime(x, ...)
-
-  ## convert to decimal hours
-  dtm <- strftime(x, "%H:%M")
-  dcm <- sapply(strsplit(dtm, ":"), function(x) {
-    x <- as.numeric(x)
-    x[1] + x[2] / 60
-  })
-
-  ## calculate hour angle (15 degrees per hour from noon)
-  (dcm - 12) * 15
-}
+  ## convert 'origin' to 'POSIXt'
+  if (!any(class(origin) == "POSIXt"))
+    origin <- strptime(origin, ...)
+            
+  ## compute pixel-wise angular declination of the sun
+  delta <- declination(as.Date(origin + raster::getValues(x)), 
+                       formula = formula[1], unit = "radians")          
+  delta <- raster::setValues(x, delta)          
+  
+  ## pixel-wise latitude
+  yrs <- raster::res(dem)[2]
+  lts <- seq(raster::ymax(dem) - .5 * yrs, raster::ymin(dem), -yrs)
+  
+  mat <- matrix(ncol = raster::ncol(dem), nrow = raster::nrow(dem))
+  for (i in 1:nrow(mat)) mat[i, ] <- lts[i]
+  
+  phi <- raster::raster(mat, template = dem) * pi / 180
+  
+  ## compute surface slope and aspect
+  slp <- raster::terrain(dem, opt = "slope", neighbors = neighbors, 
+                         unit = "radians")
+  
+  asp <- raster::terrain(dem, opt = "aspect", neighbors = neighbors, 
+                         unit = "radians")
+  asp <- raster::calc(asp, fun = function(x) x - pi)
+  
+  ## compute solar hour angle
+  omega <- solarHourAngle(x, unit = "radians")
+  
+  ## compute solar incidence angle
+  theta <- acos(sin(delta) * sin(phi) * cos(slp) - 
+                  sin(delta) * cos(phi) * sin(slp) * cos(asp) + 
+                  cos(delta) * cos(phi) * cos(slp) * cos(omega) + 
+                  cos(delta) * sin(phi) * sin(slp) * cos(asp) * cos(omega) + 
+                  cos(delta) * sin(asp) * sin(slp) * sin(omega))
+  
+  ## convert to degrees (optional) and return
+  if (unit[1] == "degrees")
+    return(theta * 180 / pi)
+  else
+    return(theta)
+})
